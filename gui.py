621994b5744eceb250ba1bc7c188252849581c2a
Schmalz67 <87285286+Schmalz67@users.noreply.github.com>
2021-07-24 17:06:54 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,43 +1,53 @@
-from tkinter import *
-#from tkinter.ttk import *
-from main import *
-#master = Tk()
-from functools import partial
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QMessageBox, QTableWidget, QTableWidgetItem)
+import pandas as pd
+class LoginForm(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle('Login Form')
+		self.resize(500, 120)
 
-def validateLogin(username, password):
-	print("username entered :", username.get())
-	print("password entered :", password.get())
-	return
+		layout = QGridLayout()
 
-#window
-tkWindow = Tk()
-tkWindow.geometry('400x150')
-tkWindow.title('Banking Form')
+		label_name = QLabel('<font size="4"> Username </font>')
+		self.lineEdit_username = QLineEdit()
+		self.lineEdit_username.setPlaceholderText('Please enter your username')
+		layout.addWidget(label_name, 0, 0)
+		layout.addWidget(self.lineEdit_username, 0, 1)
 
-#username label and text entry box
-usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
-username = StringVar()
-usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)
+		label_password = QLabel('<font size="4"> Password </font>')
+		self.lineEdit_password = QLineEdit()
+		self.lineEdit_password.setPlaceholderText('Please enter your password')
+		layout.addWidget(label_password, 1, 0)
+		layout.addWidget(self.lineEdit_password, 1, 1)
 
-#password label and password entry box
-passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)
-password = StringVar()
-passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+		button_login = QPushButton('Login')
+		button_login.clicked.connect(self.check_password)
+		layout.addWidget(button_login, 2, 0, 1, 2)
+		layout.setRowMinimumHeight(2, 75)
 
-#def openNewWindow():
-#    newWindow = Toplevel(master)
-#    newWindow.title("display")
-#    newWindow.geometry("200x200")
-#    Label(newWindow, text="user").pack()
-#label = Label(master, text="This will open a new window")
-#label.pack(pady = 10)
-
-#next = Button(master, text="Next", command = openNewWindow())
-
-#signup
-login = Button(tkWindow, text="login" , command=read_one_user).grid(row=5, column=0)
-signup = Button(tkWindow, text="Sign Up", command=insert_user).grid(row=6, column=0)
+		self.setLayout(layout)
 
 
 
-#tkWindow.mainloop()
+	def check_password(self):
+		msg = QMessageBox()
+
+		if self.lineEdit_username.text() == 'admin' and self.lineEdit_password.text() == 'password':
+			msg.setText('Welcome Drew, you have $3,094.98 in your account. ')
+			msg.exec_()
+			app.quit()
+		else:
+			msg.setText('Incorrect Password')
+			msg.exec_()
+
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+
+	form = LoginForm()
+	form.show()
+
+	sys.exit(app.exec_())
+
